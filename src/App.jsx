e9a4a78ae078f57ec4b51664fa48b29099941592
Junior-git <jddb26@gmail.com>
2021-07-17 17:48:50 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
-import Login from './pages/Login/index.js'
+import Contratos from "./components/Contratos";
 
-function App() {
+//Importar el componente(Mayuscula inicial OJO)
+// import Lista from './components/Lista';
+// import Welcome from './components/Welcome';
+// import Estado from './components/Estado';
+// import Formulario from './components/Formulario';
+
+//import Usuario from "./components/Usuario";
+//import Contratos from "./components/Contratos";
+import Formulario from "./components/Formulario";
+//import Lista from "./components/Lista"
+//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() 
+{
+  // const names = ["Pedro", "Pablo", "Juan"]
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <div className="App mt-5">
+      {/* <Usuario></Usuario> */}
+     {/* <Contratos></Contratos> */} 
+    <Formulario></Formulario> */}
+   {/* <Lista></Lista> */}
+   </div>
   );
 }
 
